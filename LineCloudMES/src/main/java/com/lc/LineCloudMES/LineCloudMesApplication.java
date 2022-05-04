388807c190ae2950c548f858f515def8eca27c72
irkstpdl <irkstpdl@163.com>
2022-05-04 19:54:47 +0800

    Lc MES Framework

---
+++ LineCloudMES/src/main/java/com/lc/LineCloudMES/LineCloudMesApplication.java
@@ -0,0 +1,13 @@
+package com.lc.LineCloudMES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineCloudMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineCloudMesApplication.class, args);
+	}
+
+}
---
+++ LineCloudMES/src/test/java/com/lc/LineCloudMES/LineCloudMesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc.LineCloudMES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineCloudMesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
