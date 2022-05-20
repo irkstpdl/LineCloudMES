af59c52c5a3d3d43edd82f3ed0c21f663a15012b
irkstpdl <irkstpdl@163.com>
2022-05-20 15:54:44 +0800

    MailServiceImpl

--- LineCloudMES/LineCloud/lc-mail/src/main/java/com/lc/mail/internal/MailServiceImpl.java
+++ LineCloudMES/LineCloud/lc-mail/src/main/java/com/lc/mail/internal/MailServiceImpl.java
@@ -23,6 +23,50 @@ public class MailServiceImpl implements MailService {
     @Autowired
     protected JavaMailSender mailSender;
 
-    @Value("${mail.adress}")
-    private
+    @Value("${mail.address}")
+    private String defaultSender;
+
+    @Override
+    public void sendEmail(final String recipient, final String subject, final String body) {
+        sendHtmlTextEmail(getDefaultSender(), recipient, subject, body);
+    }
+
+    protected String getDefaultSender() {
+        if (isValidEmail(defaultSender)) {
+            return defaultSender;
+        }
+        throw new MailConfigurationException('\'' + defaultSender + "' 不是有效的电子邮件地址，请检查！");
+    }
+
+    protected void sendHtmlTextEmail(final String sender, final String recipient, final String subject, final String body) {
+        validateEmail(sender);
+        validateEmail(recipient);
+        Preconditions.checkArgument(StringUtils.isNotBlank(subject), "电子邮件主题不应为空");
+        Preconditions.checkArgument(StringUtils.isNotBlank(body), "电子邮件正文不应为空");
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(recipient);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(body, true);
+        } catch (MessagingException e) {
+            throw new MailPreparationException(e);
+        }
+
+        mailSender.send(mimeMessage);
+    }
+
+    private void validateEmail(final String email) {
+        if (!isValidEmail(email)) {
+            throw new InvalidMailAddressException('\'' + email + "' 不是有效的电子邮件地址");
+        }
+    }
+
+    public static boolean isValidEmail(final String email) {
+        return StringUtils.isNotBlank(email) && EmailValidator.getInstance().isValid(email);
+    }
+
 }
