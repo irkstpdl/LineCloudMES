ab26e669a342782af00e89c3b0e2f0bcdd3cebba
irkstpdl <irkstpdl@163.com>
2022-05-25 22:44:35 +0800

    modelHooksCTM

--- LineCloudMES/LineCloud/lc-plugins-custom-translation-management/src/main/java/com/lc/plugins/customTranslations/internal/hooks/CustomTranslationModelHooksCTM.java
+++ LineCloudMES/LineCloud/lc-plugins-custom-translation-management/src/main/java/com/lc/plugins/customTranslations/internal/hooks/CustomTranslationModelHooksCTM.java
@@ -1,6 +1,33 @@
 package com.lc.plugins.customTranslations.internal.hooks;
 
+import static com.lc.customTranslation.constants.CustomTranslationFields.PLUGIN_IDENTIFIER;
+import static com.lc.plugins.customTranslations.constants.CustomTranslationFieldsCTM.PLUGIN_NAME;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.lc.model.api.DataDefinition;
+import com.lc.model.api.Entity;
+import com.lc.plugin.api.PluginAccessor;
+@Service
 public class CustomTranslationModelHooksCTM {
+
+    @Autowired
+    private PluginAccessor pluginAccessor;
+
+    public void updateCustomTranslationData(final DataDefinition customTranslationDD, final Entity customTranslation) {
+        String pluginIdentifier = customTranslation.getStringField(PLUGIN_IDENTIFIER);
+
+        customTranslation.setField(PLUGIN_NAME, getPluginName(pluginIdentifier));
+    }
+
+    private String getPluginName(final String pluginIdentifier) {
+        String pluginName = null;
+
+        if (pluginIdentifier != null) {
+            pluginName = pluginAccessor.getPlugin(pluginIdentifier).getPluginInformation().getName();
+        }
+
+        return pluginName;
+    }
 }
