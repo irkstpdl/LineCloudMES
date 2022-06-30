ad19e8bfbc4d44aaadc362abebfa159df4b0955b
irkstpdl <irkstpdl@163.com>
2022-06-30 17:29:02 +0800

    用户设置模块

---
+++ LineCloudMES/LineCloud/lc-plugins-user-management/src/main/java/com/lc/plugins/users/constants/GroupDetailsConstants.java
@@ -0,0 +1,11 @@
+package com.lc.plugins.users.constants;
+
+public final class GroupDetailsConstants {
+
+    private GroupDetailsConstants(){
+
+    }
+
+    public static final String GROUP_ID = "groupId";
+
+}
