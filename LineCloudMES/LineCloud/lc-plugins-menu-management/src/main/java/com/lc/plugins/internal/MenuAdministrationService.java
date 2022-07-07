109060b0a25b1eea56263431b088d6d76db3b2b8
irkstpdl <irkstpdl@163.com>
2022-07-07 20:49:45 +0800

    Update README.md

---
+++ LineCloudMES/LineCloud/lc-plugins-menu-management/src/main/java/com/lc/plugins/internal/MenuAdministrationService.java
@@ -0,0 +1,4 @@
+package com.lc.plugins.internal;
+
+public class MenuAdministrationService {
+}
---
+++ LineCloudMES/LineCloud/lc-plugins-menu-management/src/main/java/com/lc/plugins/menu/LineCloudMenuConstants.java
@@ -0,0 +1,19 @@
+package com.lc.plugins.menu;
+
+public final class LineCloudMenuConstants {
+
+    private LineCloudMenuConstants() {
+
+    }
+
+    public static final String PLUGIN_IDENTIFIER = "lcMenu";
+
+    // VIEW
+    public static final String VIEW_MENU_CATEGORIES_LIST = "menuCategoriesList";
+
+    public static final String VIEW_MENU_CATEGORY_DETAILS = "menuCategoryDetails";
+
+    public static final String VIEW_MENU_VIEW_DEFINITION_ITEMS_DETAILS = "menuViewDefinitionItemDetails";
+
+
+}
